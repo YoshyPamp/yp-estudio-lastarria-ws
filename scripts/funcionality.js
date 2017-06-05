3f89acb86ae08fb32e81ab18bfe9285c93d500be
Joshe Oñate <joshe.sonido@gmail.com>
2017-06-05 17:24:48 -0400

    Se termina Trabajos y avances en la sección de valores.

---
+++ scripts/funcionality.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  //Unite Gallery para la sección de trabajos
+  $("#galeria").unitegallery({
+  	lightbox_type: "compact",
+    tiles_justified_space_between:0,
+  	tiles_type: "nested",
+    tile_enable_image_effect:true,
+    tile_image_effect_reverse:true
+  });
+});
